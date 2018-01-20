a839b342b69c3311bcce0113940189ccafc4451d
dvolvox <duarteetraud@gmail.com>
2018-01-20 17:52:37 +0000

    Added shellscripts to automate the startup process

---
+++ 1-layer.sh
@@ -0,0 +1,2 @@
+cd ./src/host/osmocon/
+./osmocon -p /dev/ttyUSB0 -m c123xor ../../target/firmware/board/compal_e88/layer1.compalram.bin
---
+++ 2-layer.sh
@@ -0,0 +1,1 @@
+./src/host/layer23/src/mobile/mobile -i 127.0.0.1
